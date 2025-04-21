4bfc6da4d0cc4eac66426fd9263d42e7822b0cef
naimislamm <naimmislam@gmail.com>
2025-04-22 03:40:51 +0600

    added menu open close

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-
+function myFunction() {
+  var x = document.getElementById("sidebar");
+  var icon = document.getElementById("menu-btn");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+    icon.className = "fas fa-bars";
+    icon.style.fontSize = "20px";
+    icon.style.color = "white";
+  } else {
+    x.style.display = "block";
+    icon.className = "fas fa-times";
+    icon.style.fontSize = "20px";
+    icon.style.color = "white";
+    icon.innerHTML = "";
+  }
+}
